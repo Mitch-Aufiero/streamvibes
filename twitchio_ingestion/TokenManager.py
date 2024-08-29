3bbddbd4a19f3ea06b3e0741d92f73292ccb043a
Mitch-Aufiero <altermodussoftware@gmail.com>
2024-08-28 20:39:08 -0500

    token refresh

--- twitchio_ingestion/TokenManager.py
+++ twitchio_ingestion/TokenManager.py
@@ -11,8 +11,11 @@ class TokenManager:
         self.redirect_uri = redirect_uri
         self.access_token = None
         self.expires_at = 0
+        self.refresh_token = ""
 
-    def get_new_token(self):
+        self.get_init_token()
+
+    def get_init_token(self):
         """Request a new OAuth token from Twitch and store it."""
         url = "https://id.twitch.tv/oauth2/token"
         params = {
@@ -31,13 +34,13 @@ class TokenManager:
             raise Exception(f"Error occurred retrieving new twitch auth token: {response_data}")
    
 
-
+        self.refresh_token = response_data["refresh_token"]
         expires_in = response_data["expires_in"]
         self.expires_at = time.time() + expires_in
 
         self.access_token = response_data["access_token"]
 
-        print(f"New token retrieved, expires in {expires_in} seconds.")
+        print(f"Initial token retrieved, expires in {expires_in} seconds.")
 
     def get_token(self):
         """Get the current OAuth token, refreshing if necessary."""
@@ -47,3 +50,24 @@ class TokenManager:
             self.get_new_token()
 
         return self.access_token
+
+    def refresh_access_token(self):
+        """Refresh the OAuth access token using the refresh token."""
+        url = "https://id.twitch.tv/oauth2/token"
+        params = {
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "grant_type": "refresh_token",
+            "refresh_token": self.refresh_token  
+        }
+        response = requests.post(url, params=params)
+        response_data = response.json()
+
+        if response.status_code >= 400:
+            raise Exception(f"Error occurred refreshing twicth auth token: {response_data}")
+
+        self.access_token = response_data["access_token"]
+        self.refresh_token = response_data.get("refresh_token", self.refresh_token) 
+        self.expires_at = time.time() + response_data["expires_in"]
+
+        print(f"Token refreshed, new token expires in {response_data['expires_in']} seconds.")
