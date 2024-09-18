f517be5b27321e0afdc866d0adda1276726bb3ab
Mitch-Aufiero <altermodussoftware@gmail.com>
2024-09-17 20:43:34 -0500

    pulled config out, added s3 connection config to sparkJob base class

--- spark_processing/kafka_to_s3.py
+++ spark_processing/kafka_to_s3.py
@@ -1,29 +1,26 @@
+import json
+import os
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import window, col, from_json, current_timestamp
+from pyspark.sql.functions import window, col, from_json
 from pyspark.sql.types import StructType, StructField, StringType
 from utils.spark_utils import SparkJob
 
 class KafkaToS3Job(SparkJob):
-    def __init__(self, app_name: str, kafka_bootstrap_servers: str, kafka_topic: str, s3_output_path: str, s3_endpoint_url: str, s3_access_key: str, s3_secret_key: str):
-        super().__init__(app_name)
-        self.kafka_bootstrap_servers = kafka_bootstrap_servers
-        self.kafka_topic = kafka_topic
-        self.s3_output_path = s3_output_path
+    def __init__(self, app_name: str, config: dict):
+        super().__init__(app_name, config)
+        self.kafka_bootstrap_servers = config["kafka"]["bootstrap_servers"]
+        self.kafka_topic = config["kafka"]["topic"]
+        self.s3_output_path = config["s3"]["raw_bucket"]
         
-        # Set Spark configurations for DigitalOcean Spaces (S3-compatible storage)
-        self.spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", s3_endpoint_url)
-        self.spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", s3_access_key)
-        self.spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", s3_secret_key)
-        self.spark._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")  # Use path-style access for S3 API compatibility
+        
+        self.spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", config["s3"]["endpoint_url"])
+        self.spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", config["s3"]["access_key"])
+        self.spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", config["s3"]["secret_key"])
+        self.spark._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
         self.spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-        self.spark._jsc.hadoopConfiguration().set("fs.s3a.multipart.size", "104857600")  # Optional: Tune for large file sizes
+        self.spark._jsc.hadoopConfiguration().set("fs.s3a.multipart.size", "104857600")
 
     def read_from_kafka(self) -> DataFrame:
-        """
-        Reads data from a Kafka topic.
-
-        :return: A DataFrame with the streamed Kafka data.
-        """
         return self.spark.readStream \
             .format("kafka") \
             .option("kafka.bootstrap.servers", self.kafka_bootstrap_servers) \
@@ -33,27 +30,15 @@ class KafkaToS3Job(SparkJob):
             .load()
 
     def process_data(self, df: DataFrame) -> DataFrame:
-        """
-        Process Kafka data, extracting the value field and adding a timestamp.
-
-        :param df: Input DataFrame from Kafka
-        :return: Processed DataFrame with extracted fields and a timestamp column
-        """
         schema = StructType([
             StructField("channel", StringType(), True),
             StructField("message", StringType(), True)
         ])
-
         return df.selectExpr("CAST(value AS STRING)", "CAST(timestamp AS TIMESTAMP)") \
             .withColumn("json_data", from_json(col("value"), schema)) \
             .select(col("json_data.channel"), col("json_data.message"), col("timestamp"))
 
     def write_to_s3(self, df: DataFrame):
-        """
-        Write DataFrame to DigitalOcean Spaces in Parquet format, partitioned by an hourly window.
-        
-        :param df: DataFrame containing Kafka data.
-        """
         query = df.withColumn("window", window(df["timestamp"], "1 hour")) \
             .writeStream \
             .format("parquet") \
@@ -66,33 +51,21 @@ class KafkaToS3Job(SparkJob):
         query.awaitTermination()
 
     def run(self):
-        """
-        Main execution method for the job: reads from Kafka, processes, and writes to DigitalOcean Spaces.
-        """
         kafka_data = self.read_from_kafka()
         processed_data = self.process_data(kafka_data)
         self.write_to_s3(processed_data)
 
 
 if __name__ == "__main__":
-    
-    kafka_bootstrap_servers = "kafka:9092"
-    kafka_topic = "twitch_chat"
-    
-    # s3 configuration
-    s3_output_path = "s3a:"
-    s3_endpoint_url = ""
-    s3_access_key = ""
-    s3_secret_key = ""
+    # Load config.json
+    with open("/src/config.json", 'r') as config_file:
+        config = json.load(config_file)
 
-    # Initialize and run the Kafka to S3 job
+    print("Current working dir: ", os.getcwd())
+
+    
     job = KafkaToS3Job(
         app_name="KafkaToS3Job",
-        kafka_bootstrap_servers=kafka_bootstrap_servers,
-        kafka_topic=kafka_topic,
-        s3_output_path=s3_output_path,
-        s3_endpoint_url=s3_endpoint_url,
-        s3_access_key=s3_access_key,
-        s3_secret_key=s3_secret_key
+        config=config
     )
     job.run()
--- spark_processing/utils/spark_utils.py
+++ spark_processing/utils/spark_utils.py
@@ -1,37 +1,53 @@
+import json
 from pyspark.sql import SparkSession, DataFrame
 
 class SparkJob:
-    def __init__(self, app_name:str):
+    def __init__(self, app_name: str, config: dict):
         """
-        Initialize the Spark session with the given application name.
+        Initialize the Spark session and load the S3 configuration from a file.
 
         Parameters:
         app_name (str): The name of the Spark application.
+        config (dict): configuration dict that contains S3 and other settings.
         """
         self.app_name = app_name
+        self.config = config
         self.spark = self.initialize_spark_session()
+
+    def load_config(self, config_file: str) -> dict:
+        """
+        Load configuration from a JSON file.
+
+        Parameters:
+        config_file (str): Path to the JSON configuration file.
+
+        Returns:
+        dict: Configuration data loaded from the file.
+        """
+        with open(config_file, 'r') as f:
+            return json.load(f)
 
     def initialize_spark_session(self) -> SparkSession:
         """
-        Create and return a Spark session.
+        Create and return a Spark session with S3 configuration.
 
-        This method initializes a Spark session using the provided application name.
-        You can further configure the Spark session here if needed (e.g., setting memory limits,
-        enabling specific Spark configurations, etc.).
+        The S3 settings are loaded from the config file.
 
         Returns:
         SparkSession: A Spark session instance.
         """
+        
         return SparkSession.builder \
             .appName(self.app_name) \
+            .config("spark.hadoop.fs.s3a.access.key", self.config['s3']['access_key']) \
+            .config("spark.hadoop.fs.s3a.secret.key", self.config['s3']['secret_key']) \
+            .config("spark.hadoop.fs.s3a.endpoint", self.config['s3']['endpoint_url']) \
             .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.2") \
             .getOrCreate()
 
-    def load_s3_data(self, path:str) -> DataFrame:
+    def load_s3_data(self, path: str) -> DataFrame:
         """
-        Load data from a specified S3 path.
-
-        This method loads a Parquet file from an S3 path into a Spark DataFrame.
+        Load data from a specified S3 path (Parquet format).
 
         Parameters:
         path (str): The S3 path where the Parquet file is stored.
@@ -39,9 +55,10 @@ class SparkJob:
         Returns:
         DataFrame: A Spark DataFrame containing the data from the specified S3 file path.
         """
-        return self.spark.read.parquet(path)
+        full_path = f"s3a://{self.config['s3']['bucket']}/{path}"
+        return self.spark.read.parquet(full_path)
 
-    def write_s3_data(self, df:DataFrame, path:str):
+    def write_s3_data(self, df: DataFrame, path: str):
         """
         Write a Spark DataFrame to a specified S3 path in Parquet format.
 
@@ -52,14 +69,12 @@ class SparkJob:
         Returns:
         None
         """
-        df.write.parquet(path)
+        full_path = f"s3a://{self.config['s3']['bucket']}/{path}"
+        df.write.parquet(full_path)
 
     def stop_spark(self) -> None:
         """
         Stop the Spark session.
-
-        This method terminates the Spark session. It should be called after
-        all processing is complete to free up resources.
 
         Returns:
         None
